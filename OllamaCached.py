bfe5339b1ea8d6d4bdd57378e008ba5e4a68fc84
Timur O <19150752+Timur-O@users.noreply.github.com>
2024-04-24 14:57:22 +0200

    :sparkles: Add Zero + Few Shot Prompting

---
+++ OllamaCached.py
@@ -0,0 +1,84 @@
+import array
+import os
+from tqdm import tqdm
+import ollama
+
+# Set environment variable to save models to the models folder in the repository.
+os.environ["OLLAMA_MODELS"] = "./models"
+os.environ["OLLAMA_KEEP_ALIVE"] = "1m"
+
+
+def check_and_download_model(model_name: str) -> None:
+    """
+    Checks if the specified LLM exists locally and downloads it if necessary.
+
+    Args:
+        model_name (str): Tag of the LLM.
+    """
+    model_exists: bool = False
+
+    existing_models = ollama.list()["models"]
+    for model in existing_models:
+        if model["name"] == model_name:
+            model_exists = True
+
+    if not model_exists:
+        print("Model Uncached - Downloading...")
+        current_digest, bars = '', {}
+        for progress in ollama.pull(model_name, stream=True):
+            digest = progress.get('digest', '')
+            if digest != current_digest and current_digest in bars:
+                bars[current_digest].close()
+
+            if not digest:
+                print(progress.get('status'))
+                continue
+
+            if digest not in bars and (total := progress.get('total')):
+                bars[digest] = tqdm(total=total, desc=f'pulling {digest[7:19]}', unit='B', unit_scale=True)
+
+            if completed := progress.get('completed'):
+                bars[digest].update(completed - bars[digest].n)
+
+            current_digest = digest
+        print("Download Complete.")
+    else:
+        print("Model Cached - Using Cached Version...")
+
+
+def zero_shot_model(model_name: str, message: str) -> str:
+    """
+    Chat with the llm without any prior training (i.e. zero-shot)
+
+    Args:
+        model_name (str): Tag of the LLM.
+        message (str): The prompt to provide to the LLM.
+    Returns:
+        response (str): The response from the assistant.
+    """
+    check_and_download_model(model_name)
+
+    print("Zero Shot (" + model_name + "):")
+    response = ollama.chat(model=model_name, messages=[{"role": "user", "content": message}], stream=False)
+    return response["message"]["content"]
+
+
+def few_shot_model(model_name: str, training_data: array, message: str) -> str:
+    """
+    Chat with the llm without any prior training (i.e. zero-shot)
+
+    Args:
+        model_name (str): Tag of the LLM.
+        training_data (array): The training data provided as an array in the format of (input, output).
+        message (str): The prompt to provide to the LLM.
+    Returns:
+        response (str): The response from the assistant.
+    """
+    check_and_download_model(model_name)
+
+    print("Few Shot (" + model_name + "): " + str(len(training_data)) + " Samples")
+    few_shot_message: str = "\n\n".join(training_data.map(lambda pair: "Input: " + pair[0] + "\nOutput: " + pair[1]))
+    few_shot_message = few_shot_message + "\n\n" + message
+
+    response = ollama.chat(model=model_name, messages=[{"role": "user", "content": few_shot_message}], stream=False)
+    return response["message"]["content"]
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Research-Project
-Research Project: Leveraging Large Language Models for Classifying Deliberative Elements in Public Discourse.
+
+Leveraging Large Language Models for Classifying Deliberative Elements in Public Discourse.
+
+## Setup
+1. Clone Repository
+2. Setup Python Version: 3.12.0
+3. Run "pip install requirements.txt"
