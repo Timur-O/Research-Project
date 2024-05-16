9a44cb5a0878ae2c2fe266cca303de1bba986ab2
Timur O <19150752+Timur-O@users.noreply.github.com>
2024-05-16 09:53:30 +0200

    :sparkles: Add everything except fine-tuning and chain-of-thought few

--- OllamaCached.py
+++ OllamaCached.py
@@ -46,51 +46,71 @@ def check_and_download_model(model_name: str) -> None:
         print("Model Cached - Using Cached Version...")
 
 
-def zero_shot(model_name: str, message: str) -> str:
+def zero_shot(model_name: str, sys_message: str, message: str) -> str:
     """
     Chat with the llm without any prior training (i.e. zero-shot)
 
     Args:
         model_name (str): Tag of the LLM.
-        message (str): The prompt to provide to the LLM.
+        sys_message (str): The prompt to provide to the LLM as the system.
+        message (str): The prompt to provide to the LLM as the user.
     Returns:
         response (str): The response from the assistant.
     """
     check_and_download_model(model_name)
 
     print("Zero Shot (" + model_name + "):")
-    response = ollama.chat(model=model_name, messages=[{"role": "user", "content": message}], stream=False)
+
+    initial_messages = [
+        {"role": "system", "content": sys_message},
+        {"role": "user", "content": message}
+    ]
+    response = ollama.chat(model=model_name,
+                           messages=initial_messages,
+                           stream=False)
     return response["message"]["content"]
 
 
-def few_shot(model_name: str, training_data: array, message: str) -> str:
+def few_shot(model_name: str, sys_message: str, training_messages: array, message: str) -> str:
     """
     Chat with the llm and provide some training samples for few-shot learning.
 
     Args:
         model_name (str): Tag of the LLM.
-        training_data (array): The training data provided as an array in the format of (input, output).
-        message (str): The prompt to provide to the LLM.
+        sys_message (str): The prompt to provide to the LLM as the system.
+        training_messages (array): The training messages provided as (input, output) pairs.
+        message (str): The prompt to provide to the LLM as the user.
     Returns:
         response (str): The response from the assistant.
     """
     check_and_download_model(model_name)
 
-    print("Few Shot (" + model_name + "): " + str(len(training_data)) + " Samples")
-    few_shot_message: str = "\n\n".join(training_data.map(lambda pair: "Input: " + pair[0] + "\nOutput: " + pair[1]))
-    few_shot_message = few_shot_message + "\n\n" + message
+    print("Few Shot (" + model_name + "): " + str(len(training_messages)) + " Samples")
+    initial_messages = [
+        {"role": "system", "content": sys_message}
+    ]
+    for training_message in training_messages:
+        user_temp = {"role": "user", "content": training_message[0]}
+        assistant_temp = {"role": "assistant", "content": training_message[1]}
+        initial_messages = initial_messages.append(user_temp)
+        initial_messages = initial_messages.append(assistant_temp)
 
-    response = ollama.chat(model=model_name, messages=[{"role": "user", "content": few_shot_message}], stream=False)
+    initial_messages = initial_messages.append({"role": "user", "content": message})
+
+    response = ollama.chat(model=model_name,
+                           messages=initial_messages,
+                           stream=False)
     return response["message"]["content"]
 
 
-def chain_of_reasoning_zero_shot(model_name: str, message: str) -> str:
+def chain_of_reasoning_zero_shot(model_name: str, sys_message: str, message: str) -> str:
     """
     Chat with the llm without any prior training (i.e. zero-shot), but with chain of thought reasoning.
 
     Args:
         model_name (str): Tag of the LLM.
-        message (str): The prompt to provide to the LLM.
+        sys_message (str): The prompt to provide to the LLM as the system.
+        message (str): The prompt to provide to the LLM as the user.
     Returns:
         response (str): The response from the assistant.
     """
@@ -99,6 +119,7 @@ def chain_of_reasoning_zero_shot(model_name: str, message: str) -> str:
     print("Chain of Reasoning - Zero Shot (" + model_name + "):")
 
     initial_messages = [
+        {"role": "system", "content": sys_message},
         {"role": "user", "content": message},
         {"role": "assistant", "content": "Let's think step by step."}
     ]
@@ -116,31 +137,33 @@ def chain_of_reasoning_zero_shot(model_name: str, message: str) -> str:
     return response["message"]["content"]
 
 
-def chain_of_reasoning_few_shot(model_name: str, training_data: array, message: str) -> str:
+def chain_of_reasoning_few_shot(model_name: str, sys_message: str, training_messages: array, message: str) -> str:
     """
     Chat with the llm and provide some training samples for few-shot learning, but also with chain of thought reasoning.
 
     Args:
         model_name (str): Tag of the LLM.
-        training_data (array): The training data as an array in the form: (input, output), where output contains an
-                               explanation of how to get to the answer including the final answer.
-        message (str): The prompt to provide to the LLM.
+        sys_message (str): The prompt to provide to the LLM as the system.
+        training_messages (array): The training messages provided as (input, output) pairs.
+        message (str): The prompt to provide to the LLM as the user.
     Returns:
         response (str): The response from the assistant.
     """
     check_and_download_model(model_name)
 
-    print("Few Shot - Chain of Reasoning (" + model_name + "): " + str(len(training_data)) + " Samples")
-
-    few_shot_message: str = "\n\n".join(
-        training_data.map(lambda pair: "Input: " + pair[0] + "\nOutput: Let's think step by step. \n" + pair[1])
-    )
-    few_shot_message = few_shot_message + "\n\n" + message
-
+    print("Few Shot (" + model_name + "): " + str(len(training_messages)) + " Samples")
     initial_messages = [
-        {"role": "user", "content": few_shot_message},
-        {"role": "assistant", "content": "Let's think step by step."}
+        {"role": "system", "content": sys_message}
     ]
+    for training_message in training_messages:
+        user_temp = {"role": "user", "content": training_message[0]}
+        assistant_temp = {"role": "assistant", "content": training_message[1]}
+        initial_messages = initial_messages.append(user_temp)
+        initial_messages = initial_messages.append(assistant_temp)
+
+    initial_messages = initial_messages.append({"role": "user", "content": message})
+    initial_messages = initial_messages.append({"role": "assistant", "content": "Let's think step by step."})
+
     complex_response = ollama.chat(model=model_name,
                                    messages=initial_messages,
                                    stream=False)
--- sentiment/main.py
+++ sentiment/main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import csv
+from sklearn.model_selection import train_test_split
+
+import OllamaCached
 
 
 def read_data_and_create_soft_labels():
@@ -30,20 +33,55 @@ def read_data_and_create_soft_labels():
     )
 
 
-def zero_shot(input):
-    pass
+def zero_shot(model_name, input_row):
+    system_text = ("You are a sentiment analysis model. Analyze the given text and provide a probability distribution "
+                   "across five sentiment categories: Strongly Negative, Slightly Negative, Neutral, Slightly Positive,"
+                   " and Strongly Positive. Return your results as a Python list of floats, where each element "
+                   "represents the probability of the corresponding sentiment category. The sum of all probabilities "
+                   "must equal 1.0.")
+    prompt = "Textual Input: " + input_row
+    return OllamaCached.zero_shot(model_name, system_text, prompt)
 
 
-def few_shot(input):
-    pass
+def few_shot(model_name, training_data, input_row):
+    system_text = ("You are a sentiment analysis model. Analyze the given text and provide a probability distribution "
+                   "across five sentiment categories: Strongly Negative, Slightly Negative, Neutral, Slightly Positive,"
+                   " and Strongly Positive. Return your results as a Python list of floats, where each element "
+                   "represents the probability of the corresponding sentiment category. The sum of all probabilities "
+                   "must equal 1.0.")
+    prompt = "Textual Input: " + input_row
+
+    formatted_training = []
+    for training_piece in training_data:
+        formatted_training.append([training_piece[0], str(training_piece[1:6])])
+
+    return OllamaCached.few_shot(model_name, system_text, formatted_training, prompt)
 
 
-def chain_of_thought_zero(input):
-    pass
+def chain_of_thought_zero(model_name, input_row):
+    system_text = ("You are a sentiment analysis model. Analyze the given text and provide a probability distribution "
+                   "across five sentiment categories: Strongly Negative, Slightly Negative, Neutral, Slightly Positive,"
+                   " and Strongly Positive. Return your results as a Python list of floats, where each element "
+                   "represents the probability of the corresponding sentiment category. The sum of all probabilities "
+                   "must equal 1.0.")
+    prompt = "Textual Input: " + input_row
+    return OllamaCached.zero_shot(model_name, system_text, prompt)
 
 
-def chain_of_thought_few(input):
-    pass
+def chain_of_thought_few(model_name, training_data, input_row):
+    system_text = ("You are a sentiment analysis model. Analyze the given text and provide a probability distribution "
+                   "across five sentiment categories: Strongly Negative, Slightly Negative, Neutral, Slightly Positive,"
+                   " and Strongly Positive. Return your results as a Python list of floats, where each element "
+                   "represents the probability of the corresponding sentiment category. The sum of all probabilities "
+                   "must equal 1.0.")
+    prompt = "Textual Input: " + input_row
+
+    formatted_training = []
+    for training_piece in training_data:
+        # TODO - Add Reasoning
+        formatted_training.append([training_piece[0], str(training_piece[1:6])])
+
+    return OllamaCached.chain_of_reasoning_few_shot(model_name, system_text, formatted_training, prompt)
 
 
 def write_to_file(name, data):
@@ -55,25 +93,29 @@ def write_to_file(name, data):
 
 if __name__ == "__main__":
     soft_labels = read_data_and_create_soft_labels()
+    model = "llama3"
 
     zero_shot_results = []
     few_shot_results = []
     chain_of_thought_zero_results = []
     chain_of_thought_few_results = []
 
-    # TODO - Split into different sets? (train/validate/test)
-    for i in range(0, len(soft_labels)):
-        input_and_target_results = soft_labels.iloc[i].values  # 0 = input, 5 next values are target soft labels
+    # Split into different sets? (train/validate/test) - 60% training, 20% validation, 20% testing
+    train_df, temp_df = train_test_split(soft_labels, test_size=0.4, random_state=42)
+    val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
+
+    for i in range(0, len(test_df)):
+        input_and_target_results = test_df.iloc[i].values  # 0 = input, 5 next values are target soft labels
         zero_shot_predicted = [0, 0, 0, 0, 0]
         few_shot_predicted = [0, 0, 0, 0, 0]
         chain_of_thought_zero_predicted = [0, 0, 0, 0, 0]
         chain_of_thought_few_predicted = [0, 0, 0, 0, 0]
 
         for j in range(0, 3):
-            zero_shot_predicted += zero_shot(input_and_target_results[0])
-            few_shot_predicted += few_shot(input_and_target_results[0])
-            chain_of_thought_zero_predicted += chain_of_thought_zero(input_and_target_results[0])
-            chain_of_thought_few_predicted += chain_of_thought_few(input_and_target_results[0])
+            zero_shot_predicted += zero_shot(model, input_and_target_results[0])
+            few_shot_predicted += few_shot(model, train_df, input_and_target_results[0])
+            chain_of_thought_zero_predicted += chain_of_thought_zero(model, input_and_target_results[0])
+            chain_of_thought_few_predicted += chain_of_thought_few(model, train_df, input_and_target_results[0])
 
         zero_shot_predicted /= 3
         zero_shot_results.append(zero_shot_predicted)
