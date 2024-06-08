8a164b26f73d9fdddc8f719a2b1dd6d2c5bcac5e
Timur O <19150752+Timur-O@users.noreply.github.com>
2024-06-08 22:11:33 +0200

    :ok_hand: Improve Evaluation Output

--- sentiment/evaluation.py
+++ sentiment/evaluation.py
@@ -152,20 +152,23 @@ if __name__ == "__main__":
     print('Zero Shot Evaluation')
     accuracy_value_zero = accuracy(hard_true, pred_zero_hard)
     f1_value_zero = f1_score(hard_true, pred_zero_hard)
+    print('Accuracy:', accuracy_value_zero, 'F1:', f1_value_zero)
     fuzzy_accuracy_value_zero = fuzzy_accuracy(soft_true, pred_zero_soft)
     fuzzy_f1_value_zero = fuzzy_f1_score(soft_true, pred_zero_soft)
+    print('Fuzzy Accuracy:', fuzzy_accuracy_value_zero, 'Fuzzy F1:', fuzzy_f1_value_zero)
 
     # Calculate the evaluation metrics for the Few-Shot method
     print('Few Shot Evaluation')
     accuracy_value_few = accuracy(hard_true, pred_few_hard)
     f1_value_few = f1_score(hard_true, pred_few_hard)
+    print('Accuracy:', accuracy_value_few, 'F1:', f1_value_few)
     fuzzy_accuracy_value_few = fuzzy_accuracy(soft_true, pred_few_soft)
     fuzzy_f1_value_few = fuzzy_f1_score(soft_true, pred_few_soft)
+    print('Fuzzy Accuracy:', fuzzy_accuracy_value_few, 'Fuzzy F1:', fuzzy_f1_value_few)
 
     subjective_accuracy_few = []
     subjective_f1_few = []
     for i in range(0, 5):
-        print('Subjectivity', i)
         subjective_accuracy_few.append(accuracy(subj_true[:, i], pred_few_subj[:, i]))
         subjective_f1_few.append(f1_score(subj_true[:, i], pred_few_subj[:, i]))
 
@@ -173,20 +176,23 @@ if __name__ == "__main__":
     print('Cot Zero Shot Evaluation')
     accuracy_value_cot_zero = accuracy(hard_true, pred_cot_zero_hard)
     f1_value_cot_zero = f1_score(hard_true, pred_cot_zero_hard)
+    print('Accuracy:', accuracy_value_cot_zero, 'F1:', f1_value_cot_zero)
     fuzzy_accuracy_value_cot_zero = fuzzy_accuracy(soft_true, pred_cot_zero_soft)
     fuzzy_f1_value_cot_zero = fuzzy_f1_score(soft_true, pred_cot_zero_soft)
+    print('Fuzzy Accuracy:', fuzzy_accuracy_value_cot_zero, 'Fuzzy F1:', fuzzy_f1_value_cot_zero)
 
     # Calculate the evaluation metrics for the CoT Few-Shot method
     print('Cot Few Shot Evaluation')
     accuracy_value_cot_few = accuracy(hard_true, pred_cot_few_hard)
     f1_value_cot_few = f1_score(hard_true, pred_cot_few_hard)
+    print('Accuracy:', accuracy_value_cot_few, 'F1:', f1_value_cot_few)
     fuzzy_accuracy_value_cot_few = fuzzy_accuracy(soft_true, pred_cot_few_soft)
     fuzzy_f1_value_cot_few = fuzzy_f1_score(soft_true, pred_cot_few_soft)
+    print('Fuzzy Accuracy:', fuzzy_accuracy_value_cot_few, 'Fuzzy F1:', fuzzy_f1_value_cot_few)
 
     subjective_accuracy_cot_few = []
     subjective_f1_cot_few = []
     for i in range(0, 5):
-        print('Subjectivity', i)
         subjective_accuracy_cot_few.append(accuracy(subj_true[:, i], pred_cot_few_subj[:, i]))
         subjective_f1_cot_few.append(f1_score(subj_true[:, i], pred_cot_few_subj[:, i]))
 
@@ -201,8 +207,6 @@ if __name__ == "__main__":
     values_f1_soft = [fuzzy_f1_value_zero, fuzzy_f1_value_few, fuzzy_f1_value_cot_zero, fuzzy_f1_value_cot_few]
 
     # Create Accuracy Chart
-    print(values_accuracy_hard)
-    print(values_accuracy_soft)
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.bar(x - 0.2, values_accuracy_hard, 0.4, label='Hard Accuracy', color='b')
     ax.bar(x + 0.2, values_accuracy_soft, 0.4, label='Soft Accuracy', color='r')
@@ -215,8 +219,6 @@ if __name__ == "__main__":
     plt.show()
 
     # Create F1 Score Chart
-    print(values_f1_hard)
-    print(values_f1_soft)
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.bar(x - 0.2, values_f1_hard, 0.4, label='Hard F1 Score', color='b')
     ax.bar(x + 0.2, values_f1_soft, 0.4, label='Soft F1 Score', color='r')
@@ -232,8 +234,8 @@ if __name__ == "__main__":
     x = np.arange(len(labels))
 
     # Create Accuracy Chart
-    print(subjective_accuracy_few)
-    print(subjective_accuracy_cot_few)
+    print("Accuracy - Few:", subjective_accuracy_few)
+    print("Accuracy - CoT:", subjective_accuracy_cot_few)
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.bar(x - 0.2, subjective_accuracy_few, 0.4, label='Few-Shot', color='b')
     ax.bar(x + 0.2, subjective_accuracy_cot_few, 0.4, label='CoT Few-Shot', color='r')
@@ -246,8 +248,8 @@ if __name__ == "__main__":
     plt.show()
 
     # Create F1 Score Chart
-    print(subjective_f1_few)
-    print(subjective_f1_cot_few)
+    print("F1 - Few:", subjective_f1_few)
+    print("F1 - CoT Few", subjective_f1_cot_few)
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.bar(x - 0.2, subjective_f1_few, 0.4, label='Few-Shot', color='b')
     ax.bar(x + 0.2, subjective_f1_cot_few, 0.4, label='CoT Few-Shot', color='r')
